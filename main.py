2806cdd004127d81e502ebfbf10478e57708d0b1
devin-sachs <75508790+devin-sachs@users.noreply.github.com>
2024-12-19 11:50:19 -0800

    Contious work on iterating delivery and sorting

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ import truck
 import datetime
 
 distance_data = []
-address_data  = []
+address_data = []
 
 hash_table = hash.ChainingHashTable()
+
 
 def load_package_data():
     with open("WGUPS Package File-cleanup.csv") as packageCSV:
@@ -29,10 +30,11 @@ def load_package_data():
 
             #insert into hash table
 
-            hash_table.insert(package_id,all_packages)
+            hash_table.insert(package_id, all_packages)
 
 
 load_package_data()
+
 
 #validating hash table and that packages were inserted properly
 
@@ -54,10 +56,11 @@ def load_distance_data():
 
             distance_data.append(row)
 
-
             #insert into hash table
 
+
 load_distance_data()
+
 
 #validate distance data
 # print(distance_data[5][2])
@@ -68,130 +71,135 @@ load_distance_data()
 
 
 def load_address_data():
-    with open("WGUPS Address Table-cleaned.csv" , encoding='utf-8-sig') as address_CSV_file:
+    with open("WGUPS Address Table-cleaned.csv", encoding='utf-8-sig') as address_CSV_file:
         csv_reader = csv.reader(address_CSV_file, delimiter=',')
         for row in csv_reader:
-            address_data.extend(row) #using extend to only have a one-dimensional list
+            address_data.extend(row)  #using extend to only have a one-dimensional list
+
 
 load_address_data()
+
 
 # validate address data
 # print(address_data)
 
 #returns x,y distance value from distance table
-def distance_between(address1,address2):
+def distance_between(address1, address2):
     address1 = address_data.index(address1)
     address2 = address_data.index(address2)
     return distance_data[address1][address2]
 
+
 #validate distance between function
 # print(distance_between("4001 South 700 East","177 W Price Ave"))
-
-def min_distance(from_address,truck_packages):
-    for truck_package in truck_packages:
-        return distance_between(from_address, truck_packages.packages[truck_package.address])
 
 
 truck1 = truck.Truck(1)
 truck2 = truck.Truck(2)
 truck3 = truck.Truck(3)
 
-
 # TO DO: More work to do here, see if this logic is feasible to continue.
 #try to sort packages by deadlines, notes and priority first then load them into the trucks create seperate lists????
-package_set_truck_one = set()
-package_set_truck_two = set()
-package_set_truck_three = set()
-package_set_truck_anywhere = set()
-
-
-for package in range(1,41):
-    package_object = hash_table.search(package)
-    if "Delayed" in package_object.special_notes:
-        package_set_truck_three.add(package_object)
-    if "truck 2" in package_object.special_notes:
-        package_set_truck_two.add(package_object)
-    if package_object.delivery_deadline == "9:00 AM":
-        package_set_truck_one.add(package_object)
-    elif package_object.delivery_deadline == "10:30 AM":
-        package_set_truck_two.add(package_object)
-    else:
-        package_set_truck_anywhere.add(package_object)
-
-print("Truck 1: ")
-for package in package_set_truck_one:
-    print(package)
-
-print("\nTruck 2: ")
-for package in package_set_truck_two:
-    print(package)
-
-print("\nTruck 3: ")
-for package in package_set_truck_three:
-    print(package)
-
-print("\nTruck needed: ")
-for package in package_set_truck_anywhere:
-    print(package)
+# package_set_truck_one = set()
+# package_set_truck_two = set()
+# package_set_truck_three = set()
+# package_set_truck_anywhere = set()
+#
+# for package in range(1, 41):
+#     package_object = hash_table.search(package)
+#     if "Delayed" in package_object.special_notes:
+#         package_set_truck_three.add(package_object)
+#     elif package_object.delivery_deadline == "9:00 AM":
+#         package_set_truck_two.add(package_object)
+#     elif package_object.delivery_deadline == "10:30 AM":
+#         package_set_truck_two.add(package_object)
+#     elif "truck 2" in package_object.special_notes:
+#         package_set_truck_two.add(package_object)
+#     else:
+#         package_set_truck_anywhere.add(package_object)
+#
+#
+# print("Truck 1: ")
+# for package in package_set_truck_one:
+#     truck1.load_package(package)
+#     print(package)
+#
+# print("\nTruck 2: ")
+# for package in package_set_truck_two:
+#     truck2.load_package(package)
+#     print(package)
+#
+# print("\nTruck 3: ")
+# for package in package_set_truck_three:
+#     truck3.load_package(package)
+#     print(package)
+#
+# print("\nTruck needed: ")
+# for package in package_set_truck_anywhere:
+#     print(package)
 
 # Manually loading packages in trucks
 
 def load_all_trucks():
     # Loading truck 3 with certain special notes packages
-    truck3.load_package(hash_table.search("9"))  # package has wrong address
-    truck3.load_package(hash_table.search("6"))  # some don't arrive until 9:05am
-    truck3.load_package(hash_table.search("25"))  # the rest don't fit with the rest of the trucks
-    truck3.load_package(hash_table.search("28"))
-    truck3.load_package(hash_table.search("32"))
+    truck3.load_package(hash_table.search(21))  # package has wrong address
+    truck3.load_package(hash_table.search(28))  # some don't arrive until 9:05am
+    truck3.load_package(hash_table.search(7))  # the rest don't fit with the rest of the trucks
+    truck3.load_package(hash_table.search(33))
+    truck3.load_package(hash_table.search(9))
 
-    truck3.load_package(hash_table.search("27"))
-    truck3.load_package(hash_table.search("33"))
-    truck3.load_package(hash_table.search("35"))
-    truck3.load_package(hash_table.search("39"))
+    truck3.load_package(hash_table.search(6))
+    truck3.load_package(hash_table.search(32))
+    truck3.load_package(hash_table.search(25))
+    truck3.load_package(hash_table.search(26))
 
-    truck3.load_package(hash_table.search("2"))
-    truck3.load_package(hash_table.search("4"))
+    # truck3.load_package(hash_table.search("2"))
+    # truck3.load_package(hash_table.search("4"))
 
     # Loading truck 2
-    truck2.load_package(hash_table.search("3"))  # packages must be on truck 2 or delivered together
+    truck2.load_package(hash_table.search(3))  # packages must be on truck 2 or delivered together
 
-    truck2.load_package(hash_table.search("1"))
-    truck2.load_package(hash_table.search("18"))
-    truck2.load_package(hash_table.search("36"))
-    truck2.load_package(hash_table.search("38"))
+    truck2.load_package(hash_table.search(5))
+    truck2.load_package(hash_table.search(38))
+    truck2.load_package(hash_table.search(24))
+    truck2.load_package(hash_table.search(22))
 
-    truck2.load_package(hash_table.search("13"))
-    truck2.load_package(hash_table.search("14"))
-    truck2.load_package(hash_table.search("15"))
-    truck2.load_package(hash_table.search("16"))
+    truck2.load_package(hash_table.search(36))
+    truck2.load_package(hash_table.search(12))
+    truck2.load_package(hash_table.search(17))
+    truck2.load_package(hash_table.search(11))
 
-    truck2.load_package(hash_table.search("19"))
-    truck2.load_package(hash_table.search("20"))
-    truck2.load_package(hash_table.search("29"))
-    truck2.load_package(hash_table.search("30"))
+    truck2.load_package(hash_table.search(23))
+    truck2.load_package(hash_table.search(27))
+    truck2.load_package(hash_table.search(35))
+    truck2.load_package(hash_table.search(18))
+
+    truck2.load_package(hash_table.search(10))
 
     # Loading truck 1
-    truck1.load_package(hash_table.search("31"))
-    truck1.load_package(hash_table.search("34"))
-    truck1.load_package(hash_table.search("37"))
-    truck1.load_package(hash_table.search("40"))
+    truck1.load_package(hash_table.search(1))
+    truck1.load_package(hash_table.search(40))
+    truck1.load_package(hash_table.search(4))
+    truck1.load_package(hash_table.search(19))
 
-    truck1.load_package(hash_table.search("5"))
-    truck1.load_package(hash_table.search("7"))
-    truck1.load_package(hash_table.search("8"))
-    truck1.load_package(hash_table.search("10"))
+    truck1.load_package(hash_table.search(20))
+    truck1.load_package(hash_table.search(29))
+    truck1.load_package(hash_table.search(30))
+    truck1.load_package(hash_table.search(8))
 
-    truck1.load_package(hash_table.search("11"))
-    truck1.load_package(hash_table.search("12"))
-    truck1.load_package(hash_table.search("17"))
-    truck1.load_package(hash_table.search("21"))
+    truck1.load_package(hash_table.search(37))
+    truck1.load_package(hash_table.search(31))
+    truck1.load_package(hash_table.search(39))
+    truck1.load_package(hash_table.search(13))
 
-    truck1.load_package(hash_table.search("22"))
-    truck1.load_package(hash_table.search("23"))
-    truck1.load_package(hash_table.search("24"))
-    truck1.load_package(hash_table.search("26"))
+    truck1.load_package(hash_table.search(14))
+    truck1.load_package(hash_table.search(15))
+    truck1.load_package(hash_table.search(16))
+    truck1.load_package(hash_table.search(34))
 
+#
 load_all_trucks()
+
 
 #Validate the packages for a given truck
 # for current_package in truck1.packages:
@@ -204,49 +212,75 @@ load_all_trucks()
 
 #Flush this part out afterward when packages are sorted slightly better
 
+#Sort packages once loaded inside truck
+def nearest_neighbor(truck_packages):
+    for truck_package in truck_packages:
+        return distance_between(truck_packages.packages[truck_package.address])
+
 def deliver_packages(truck):
     route_mileage = 0
-    time = datetime.timedelta(hours = 8, minutes = 0, seconds = 0)
+    time = datetime.timedelta(hours=8, minutes=0, seconds=0)
     print(time)
     for current_package in truck.packages:
         current_package.set_status("en route")
-        if current_package.delivery_deadline == "9:00 AM":
-            route_mileage += distance_between(truck.current_address,current_package.address)
-            package_time = distance_between(truck.current_address,current_package.address) / 18
-            time_to_add = datetime.timedelta(hours = package_time)
-            time += time_to_add
-            current_package.delivery_time = time
-            truck.current_address = current_package.address
-            current_package.set_status("delivered")
+        route_mileage += distance_between(truck.current_address, current_package.address)
+        package_time = distance_between(truck.current_address, current_package.address) / truck.speed
+        time_to_add = datetime.timedelta(hours=package_time)
+        time += time_to_add
+        current_package.delivery_time = time
+        truck.current_address = current_package.address
+        current_package.set_status("delivered")
+        print(time)
 
-        elif current_package.delivery_deadline == "10:30 AM":
-            route_mileage += distance_between(truck.current_address,current_package.address)
-            package_time = distance_between(truck.current_address,current_package.address) / 18
-            time_to_add = datetime.timedelta(hours = package_time)
-            time += time_to_add
-            current_package.delivery_time = time
-            truck.current_address = current_package.address
-            current_package.set_status("delivered")
-
-        elif current_package.delivery_deadline == "EOD":
-            route_mileage += distance_between(truck.current_address,current_package.address)
-            package_time = distance_between(truck.current_address,current_package.address) / 18
-            time_to_add = datetime.timedelta(hours = package_time)
-            time += time_to_add
-            current_package.delivery_time = time
-            truck.current_address = current_package.address
-            current_package.set_status("delivered")
+        # if current_package.delivery_deadline == "9:00 AM":
+        #     route_mileage += distance_between(truck.current_address, current_package.address)
+        #     package_time = distance_between(truck.current_address, current_package.address) / truck.speed
+        #     time_to_add = datetime.timedelta(hours=package_time)
+        #     time += time_to_add
+        #     current_package.delivery_time = time
+        #     truck.current_address = current_package.address
+        #     current_package.set_status("delivered")
+        #     print(time)
+        #
+        # elif current_package.delivery_deadline == "10:30 AM":
+        #     route_mileage += distance_between(truck.current_address, current_package.address)
+        #     package_time = distance_between(truck.current_address, current_package.address) / truck.speed
+        #     time_to_add = datetime.timedelta(hours=package_time)
+        #     time += time_to_add
+        #     current_package.delivery_time = time
+        #     truck.current_address = current_package.address
+        #     current_package.set_status("delivered")
+        #
+        # elif current_package.delivery_deadline == "EOD":
+        #     route_mileage += distance_between(truck.current_address, current_package.address)
+        #     package_time = distance_between(truck.current_address, current_package.address) / truck.speed
+        #     time_to_add = datetime.timedelta(hours=package_time)
+        #     time += time_to_add
+        #     current_package.delivery_time = time
+        #     truck.current_address = current_package.address
+        #     current_package.set_status("delivered")
 
     return route_mileage
 
-# print(deliver_packages(truck1))
-# print(deliver_packages(truck2))
-# print(deliver_packages(truck3))
+
+print(deliver_packages(truck1))
+print(deliver_packages(truck2))
+print(deliver_packages(truck3))
+
 
 # for current_package in truck2.packages:
 #     print(distance_between(current_package.address,truck2.current_address))
 #
+# for current_package in truck3.packages:
+#     current_package.set_status("en route")
+#     print(current_package)
+#
+# for current_package in truck2.packages:
+#     current_package.set_status("en route")
+#     print(current_package)
+#
 # for current_package in truck1.packages:
+#     current_package.set_status("en route")
 #     print(current_package)
 
 # hours = distance / speed
--- truck.py
+++ truck.py
@@ -1,7 +1,7 @@
 class Truck:
     def __init__(self,truck_id,current_address = "4001 South 700 East", speed = 18):
         self.truck_id = truck_id
-        self.packages = set() #packages in a truck are a set to help avoid any duplication
+        self.packages = [] #packages in a truck are a set to help avoid any duplication
         self.current_address = current_address
         self.speed = speed  # 18 miles per hour average speed of truck
 
@@ -9,7 +9,7 @@ class Truck:
         return "%s, %s" % (self.truck_id, self.packages)
 
     def load_package(self,package):
-        self.packages.add(package)
+        self.packages.append(package)
 
     def set_address(self,address):
         self.current_address = address
